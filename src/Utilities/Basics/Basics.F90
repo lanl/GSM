4763b93e5f09bbaa05b43a88a62d835b84e86fcb
Chase M Juneau <junechas@isu.edu>
2021-05-25 10:40:48 -0600

    Added a 'Types' module. Wrapped in a 'Basics' library for GSM
    Compiles fine with GNU 4.8. Not yet incoroporated into GSM.

---
+++ src/Utilities/Basics/Basics.F90
@@ -0,0 +1,41 @@
+! =============================================================================
+!>
+!> \file
+!> \brief  Contains the Basics module
+!> \author CMJ
+!>
+!> The Basics module contains the absolute basic utilities that are used
+!> in GSM. These utilities include things like abstracted data types, basic
+!> string manipulation, and so forth. The module facilitates ease of use for
+!> it's consumers. The module wraps several of these basic sub-modules
+!> together to provide it's API / usage.
+!>
+! =============================================================================
+
+#include "Types.F90"
+#include "LiteStrings.F90"
+
+! =============================================================================
+!>
+!> \module
+!> \brief  Provides basic abstraction and functionality for simple tasks
+!> \author CMJ
+!>
+!> The Basics module contains the absolute basic utilities that are used
+!> in GSM. These utilities include things like abstracted data types, basic
+!> string manipulation, and so forth. The module facilitates ease of use for
+!> it's consumers. The module wraps several of these basic sub-modules
+!> together to provide it's API / usage.
+!>
+! =============================================================================
+
+module Basics
+
+    use Types
+  
+  implicit none
+  public
+
+
+end module Basics
+
---
+++ src/Utilities/Basics/LiteStrings.F90
@@ -0,0 +1,19 @@
+! =============================================================================
+!>
+!> \file
+!> \brief  Contains the LiteStrings module
+!> \author CMJ
+!>
+!> The LiteStrings module contains functionality to build messages, convert
+!> basic numbers to a string format, and other similar functionality as
+!> required for GSM usage.
+!>
+! =============================================================================
+module LiteStrings
+
+    implicit none
+    private
+
+
+end module LiteStrings
+
