32f1aff61bf1a2eb4ab012cb2fbbd2fde5bff14c
Chase M Juneau <junechas@isu.edu>
2023-05-01 21:04:04 -0600

    Added start of types module being researched

---
+++ src/Harness/Types.f90
@@ -0,0 +1,23 @@
+!==============================================================================
+!> Harness Types library
+!>
+!> Library to alias different data types used by simulations / models.
+!> This library will help centralize declaration and simplify consumer usage
+!> if a consumer desired C-interoperability.
+!>
+!> All types should be publicly available.
+!
+!==============================================================================
+module hrnTypes
+
+   use, intrinsic :: iso_fortran_env
+   use, intrinsic :: iso_c_binding
+   implicit none
+   public
+
+
+contains
+
+
+end module hrnTypes
+
