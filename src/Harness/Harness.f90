65b47812db0ade1fc26fd20029811fdaa67ce255
Chase M Juneau <junechas@isu.edu>
2023-05-01 21:04:20 -0600

    Adding harness module wrapper for simple interfacing

---
+++ src/Harness/Harness.f90
@@ -0,0 +1,27 @@
+!==============================================================================
+!> Harness library
+!>
+!> The Harness library contains the essential and basic tools used by the
+!> majority of the other libraries. The tool is intended to provide robust yet
+!> simple interfaces for a variety of simple tasks. Some such task include:
+!>    + Constant Declarations
+!>    + Basic Contract functionality (e.g., input/output validation, etc.)
+!>    + FException (VERY basic emergency exit functionality)
+!>    + Testing functionality (for built-in testing - not used in simulations)
+!>
+!> Note that this interface is meant to simply "pull together" all the harness
+!> library components. It should focus on being clear, simple, and effective
+!> when used by consumers.
+!
+!==============================================================================
+module Harness
+
+   implicit none
+   public
+
+
+contains
+
+
+end module Harness
+
